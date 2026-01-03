6807301ac30bf3e5a7e0bab6607998a29f3911b9
devNicolasAmaral <nicolas.o.amaral@gmail.com>
2026-01-03 14:55:00 -0300

    Refactor: Migração de CSV para SQLite

--- investimento.py
+++ investimento.py
@@ -1,9 +1,34 @@
+import sqlite3
 from dataclasses import dataclass
 from datetime import datetime
-import csv
-import os
 import yfinance as yf
 import contextlib
+
+DB_NAME = "investimentos.db"
+
+def _conectar():
+    return sqlite3.connect(DB_NAME)
+
+def _criar_tabelas():
+    conn = _conectar()
+    cursor = conn.cursor()
+   
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS transacoes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        data TEXT NOT NULL,
+        ticker TEXT NOT NULL,
+        quantidade INTEGER NOT NULL,
+        preco REAL NOT NULL,
+        tipo TEXT NOT NULL
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+# Executa a criação da tabela quando esse módulo é importado pelo dashboard
+_criar_tabelas()
 
 @dataclass
 class Transacao:
@@ -13,52 +38,50 @@ class Transacao:
     preco: float
     tipo: str
 
-ARQUIVO_DADOS = "carteira.csv"
-
-# Função para limpar o terminal
-def limpar_tela():
-    if os.name == 'nt':
-        os.system('cls')
-    else:
-        os.system('clear')
-
-# Função para salvar as transações em um arquivo CSV
 def salvar_transacao(t: Transacao):
-    arquivo_existe = os.path.isfile(ARQUIVO_DADOS)
+    conn = _conectar()
+    cursor = conn.cursor()
 
-    with open(ARQUIVO_DADOS, mode = 'a', newline = '', encoding = 'utf-8') as f:
-        writer = csv.writer(f)
+    # Como o SQLite não tem tipo para data, padronizei como texto ISO (YYYY-MM-DD)
+    # para queo "ORDER BY DATA" funcione
+    if isinstance(t.data, (datetime, float)):
+        data_str = str(t.data)
+    else:
+        # Se vier do st.date_input (objeto date), o str() já converte para o formato certo
+        data_str = str(t.data)
 
-        if not arquivo_existe:
-            writer.writerow(['Data', 'Ticker', 'Quantidade', 'Preco', 'Tipo'])
+    cursor.execute("""
+        INSERT INTO transacoes (data, ticker, quantidade, preco, tipo)
+        VALUES (?, ?, ?, ?, ?)
+    """, (data_str, t.ticker, t.quantidade, t.preco, t.tipo))
 
-        writer.writerow([t.data, t.ticker, t.quantidade, t.preco, t.tipo])
-    
-    print(f"Transação de {t.ticker.upper()} salva no disco com sucesso!")
+    conn.commit()
+    conn.close()
 
-# Função para ler as transações e retornar em seus tipos corretos
+    print(f"Log: {t.tipo} de {t.ticker} registrada no Banco de Dados")
+
 def ler_transacoes():
-    lista_transacoes = []
+    transacoes = []
+    conn= _conectar()
+    cursor = conn.cursor()
 
-    if not os.path.isfile(ARQUIVO_DADOS):
-        return lista_transacoes
+    cursor.execute("SELECT data, ticker, quantidade, preco, tipo FROM transacoes ORDER BY data ASC")
+    linhas = cursor.fetchall()
+
+    for row in linhas:
+        t = Transacao(
+            data=row[0],
+            ticker=row[1],
+            quantidade = int(row[2]),
+            preco = float(row[3]),
+            tipo = row[4] 
+        )
+
+        transacoes.append(t)
     
-    with open(ARQUIVO_DADOS, mode = 'r', encoding = 'utf-8') as f:
-        reader = csv.DictReader(f) # Para usar o cabeçalho automaticamente
+    conn.close()
+    return transacoes
 
-        for row in reader:
-            t = Transacao(
-                data=row['Data'],
-                ticker=row['Ticker'],
-                quantidade=int(row['Quantidade']),
-                preco=float(row['Preco']),         
-                tipo=row['Tipo']
-            )
-            lista_transacoes.append(t)
-
-    return lista_transacoes
-
-# Função para cálculo da posição e preço médio
 def calcular_carteira(transacoes):
     carteira = {}
 
@@ -66,197 +89,80 @@ def calcular_carteira(transacoes):
         ticker = t.ticker
 
         if ticker not in carteira:
-            carteira[ticker] = {'qtde': 0, 'pm': 0.0}
-        
+            carteira[ticker] = {'qtde': 0, 'pm': 0.0, 'total_investido': 0.0}
+
         posicao = carteira[ticker]
 
         if t.tipo == 'C':
-            total_atual = posicao['qtde'] * posicao['pm']
+            # Na compra: Aumenta quantidade e recalculamos o preço médio (pm).
             total_novo = t.quantidade * t.preco
-            nova_qtde = posicao['qtde'] + t.quantidade
+            posicao['total_investido'] += total_novo
+            posicao['qtde'] += t.quantidade
 
-            if nova_qtde > 0:
-                novo_pm = (total_atual + total_novo) / nova_qtde
-                posicao['pm'] = novo_pm
-                posicao['qtde'] = nova_qtde
+            if posicao['qtde'] > 0:
+                posicao['pm'] = posicao['total_investido'] / posicao['qtde']
         elif t.tipo == 'V':
+            # Na venda: Diminui quantidade e abate o valor investido.
+            # O pm não muda na venda.
+            custo_venda = t.quantidade * posicao['pm']
+            posicao['total_investido'] -= custo_venda
             posicao['qtde'] -= t.quantidade
 
-    return {k: v for k, v in carteira.items() if v['qtde'] > 0}
+        # Se zerou a posição, reseta valores para evitar erros de arredondamento
+        if posicao['qtde'] == 0:
+            posicao['pm'] = 0.0
+            posicao['total_investido'] = 0.0
 
-# função para pegar os preços na B3 pelo yahoo finance 
+        # Filtro  para retornar só quem tem saldo positivo.
+        return {k: v for k, v in carteira.items() if v['qtde'] > 0}
+    
+
+# Busca cotação em lote no Yahoo Finance (Integração com API)    
 def obter_precos_atuais(lista_tickers):
-    print(f"Buscando cotações para: {lista_tickers}...")
-
+    if not lista_tickers:
+        return {}
+    
+    # O Yahoo precisa do sufixo .SA para ações brasileiras
     mapa_tickers = {f"{t}.SA": t for t in lista_tickers}
-
     tickers_yf = list(mapa_tickers.keys())
-    try:
-        dados = yf.download(tickers_yf, period = '1d', progress = False) ['Close']
 
+    try:
+        # period='1d' pega o último fechamento disponível,
+        dados = yf.download(tickers_yf, period = '1d', progress = False) ['Close']
         precos = {}
 
+        # O Pandas muda o formato se for 1 ativo (Series) ou vários (DataFrame)
         if len(tickers_yf) == 1:
-            ticker_sujo = tickers_yf[0]
-            ticker_limpo = mapa_tickers[ticker_sujo]
-
-            valor = float(dados.iloc[-1])
+            try:
+                # Tenta pegar o valor direto
+                valor = float(dados.iloc[-1])
+            except:
+                # Fallback: as vezes o yfinance devolve um DataFrame de 1 célula
+                valor = float(dados.iloc[-1].iloc[0])
+            
+            ticker_limpo = mapa_tickers[tickers_yf[0]]
             precos[ticker_limpo] = valor
         else:
+            # Loop para pegar a última linha de cada coluna
             ultima_linha = dados.iloc[-1]
             for ticker_sujo, valor in ultima_linha.items():
                 if ticker_sujo in mapa_tickers:
                     ticker_limpo = mapa_tickers[ticker_sujo]
                     precos[ticker_limpo] = float(valor)
-
+        
         return precos
-    
     except Exception as e:
-        print(f" Erro ao buscar dados: {e}")
+        print(f"erro na APT Yahoo Finance {e}")
         return {}
-
-# Função para exibir o relatório
-def exibir_relatorio(carteira):
-    if not carteira:
-        print("Carteira vazia.")
-        return
     
-    tickers = list(carteira.keys())
 
-    precos_mercado = obter_precos_atuais(tickers)
-
-    print("\n" + "=" * 60)
-    print(f"{'ATIVO':<10} | {'QTDE':<6} | {'PM (R$)':<10} | {'ATUAL (R$)':<10} | {'LUCRO (R$)':<12}")
-    print("-" * 60)
-
-    total_investido = 0
-    total_atual = 0
-
-    for ticker, dados in carteira.items():
-        qtde = dados['qtde']
-        pm = dados['pm']
-
-        preco_atual = precos_mercado.get(ticker, 0.0)
-
-        valor_posicao_compra = qtde * pm
-        valor_posicao_atual = qtde * preco_atual
-        
-        lucro_rs = valor_posicao_atual - valor_posicao_compra
-        lucro_percent = ((preco_atual / pm) - 1) * 100 if pm > 0 else 0
-
-        total_investido += valor_posicao_compra
-        total_atual += valor_posicao_atual
-
-        print(f"{ticker:<10} | {qtde:<6} | {pm:<10.2f} | {preco_atual:<10.2f} | {lucro_rs:>10.2f} ({lucro_percent:>6.2f}%)")
-
-    print("-" * 60)
-    print(f"TOTAL INVESTIDO: R$ {total_investido:,.2f}")
-    print(f"PATRIMÔNIO HOJE: R$ {total_atual:,.2f}")
-    
-    lucro_total = total_atual - total_investido
-    print(f"RESULTADO GERAL: R$ {lucro_total:,.2f}")
-    print("="*60 + "\n")
-
-# Função para validar a existencia de um ticker
+# Verifica se o ticker existe na B3 antes de deixar salvar.    
 def validar_ticker(ticker_limpo):
-    print(f"Verificando {ticker_limpo} na B3...")
-
     ticker_sujo = f"{ticker_limpo}.SA"
-
     try:
+        # contextlib para esconder logs do yfinance
         with contextlib.redirect_stdout(None), contextlib.redirect_stderr(None):
-            hist = yf.download(ticker_sujo, period = "1d", progress = False)
-
-        if len(hist) > 0:
-            return True
-        else:
-            return False
-        
+            hist = yf.download(ticker_sujo, period= '1d', progress = False)
+        return len(hist) > 0
     except Exception:
         return False
-
-# Função para Interface do Sistema
-def solicitar_dados_operacao():
-    print("\n--- Nova Operação ---")
-
-    ticker = input("Digite o Ticker (ex: PETR4): ").strip().upper()
-    ticker = ticker.replace(".SA", "")
-
-    if not ticker or len(ticker) < 3 or ticker.isnumeric():
-        print("Erro: Formato de ticker inválido.")
-        input("Pressione [ENTER] para tentar novamente...")
-        return None, None, None
-    
-    if not validar_ticker(ticker):
-        print(f"Erro: O ativo '{ticker}' não foi encontrado na bolsa.")
-        print("Dica: Verifique a digitação ou se é um ativo listado na B3.")
-        input("Pressione [ENTER] para tentar novamente...")
-        return None, None, None
-    
-    try:
-        qtde = int(input(f"Quantidade de {ticker}: "))
-        if qtde <= 0:
-            print("A quantidade deve ser maior que zero.")
-            input("Pressione [ENTER] para tentar novamente...")
-            return None, None, None
-        
-        preco = float(input("Preço Unitário: "))
-        if preco < 0:
-            print("O preço não pode ser negativo.")
-            input("Pressione [ENTER] para tentar novamente...")
-            return None, None, None
-        
-    except ValueError:
-        print("Erro: Quantidade deve ser número inteiro e o preço deve separar as casas decimais com ponto.")
-        input("Pressione [ENTER] para tentar novamente...")
-        return None, None, None
-
-    return ticker, qtde, preco
-
-if __name__ == "__main__":
-    while True:
-        limpar_tela()
-
-        print("\n" + "=" * 40)
-        print("     GESTOR DE INVESTIMENTOS (CLI)")
-        print("=" * 40)
-        print("[1] Registrar COMPRA")
-        print("[2] Registrar VENDA")
-        print("[3] Ver Carteira Atualizada")
-        print("[4] Sair")
-
-        opcao = input("Escolha uma opção: ")
-
-        if opcao == "1" or opcao == "2":
-            ticker, qtde, preco = solicitar_dados_operacao()
-
-            if ticker:
-                tipo_operacao = "C" if opcao == "1" else "V"
-
-                nova_t = Transacao(
-                    data = datetime.now(),
-                    ticker = ticker, 
-                    quantidade = qtde,
-                    preco = preco,
-                    tipo = tipo_operacao
-                )
-
-                salvar_transacao(nova_t)
-                input("\n Pressione [ENTER] para continuar...")
-        elif opcao == "3":
-            print("\n Carregando dados do disco e da web...")
-
-            historico = ler_transacoes()
-            carteira = calcular_carteira(historico)
-
-            limpar_tela()
-            exibir_relatorio(carteira)
-            
-            input("\nPressione [ENTER] para voltar ao menu...")
-
-        elif opcao == "4":
-            print("Encerrando sistema. Bons investimentos!")
-            break
-        else:
-            print("Opção inválida Digite 1, 2, 3 ou 4.")
-    
